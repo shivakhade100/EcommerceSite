15c33ef3fee1fa5dd97c0c30c95cf424a86f536a
Siddhesh Mankar <mankarsiddhesh732@gmail.com>
2025-03-31 14:28:02 +0530

    31/03/4

--- src/Billpage.jsx
+++ src/Billpage.jsx
@@ -6,15 +6,14 @@ export default function Billpage(props) {
   let { bill } = props;
   const currentDate = new Date().toLocaleDateString();
   const formatCurrency = (amount) => {
-    return amount.toLocaleString('en-IN', {
+    return amount.toLocaleString("en-IN", {
       maximumFractionDigits: 2,
-      minimumFractionDigits: 2
+      minimumFractionDigits: 2,
     });
   };
   const getDiscountedPrice = (mrp, discount) => {
-    return mrp - (mrp * discount / 100);
+    return mrp - (mrp * discount) / 100;
   };
-
 
   // if (flagLoader) {
   //   return (
@@ -47,19 +46,22 @@ export default function Billpage(props) {
             <div className="row ">
               <div className="col-3   col-lg-3 text-start ps-0 h6">Product</div>
               <div className="col-2  col-lg-3 h6  text-start ps-0">Rate</div>
-              <div className="col-3 col-lg-3 text-start   h6 text-center  ps-0    ">Quantity</div>
-              <div className="col-3 col-lg-3 text-start  ps-lg-5  h6 ">Total</div>
+              <div className="col-3 col-lg-3 text-start   h6 text-center  ps-0    ">
+                Quantity
+              </div>
+              <div className="col-3 col-lg-3 text-start  ps-lg-5  h6 ">
+                Total
+              </div>
             </div>
 
             {bill.soldProducts.map((e, index) => {
               const discountedPrice = getDiscountedPrice(e.mrp, e.discount);
               const totalPrice = discountedPrice * e.qty;
               return (
-                
                 <div className="row " key={index}>
-                  <div className="col-2  col-lg-3  text-start h6">{`${index + 1}) ${
-                    e.name
-                  }`}</div>
+                  <div className="col-2  col-lg-3  text-start h6">{`${
+                    index + 1
+                  }) ${e.name}`}</div>
                   <div className="col-4  ps-lg-0 col-lg-3 text-start ">
                     <div className="h6">
                       Rs.{" "}
@@ -73,7 +75,7 @@ export default function Billpage(props) {
                     </div>
                   </div>
                   <div className="col-2  col-lg-3 ps-lg-5 mx-lg-4  text-start h6  ps-0 ">
-                    {e.qty} 
+                    {e.qty}
                   </div>
                   <div className="col-4  col-lg-2 ps-lg-0 text-start h6">
                     {/* {e.mrp - e.mrp * (e.discount / 100)} */}
@@ -84,11 +86,12 @@ export default function Billpage(props) {
             })}
             <div className="row  my-1">
               <div className="col-9  text-end  col-lg-9 h5">Grand Total : </div>
-              <div className="col-3 col-lg- text-start  ps-0 h5">
+              <div className="col-3 col-lg- text-start  ps-0 align-items-center  my-1 h6">
                 {/* Rs. {bill.amount.toFixed(0)}{" "} */}
-                Rs{bill.amount.toLocaleString('en-IN', {
+                Rs
+                {bill.amount.toLocaleString("en-IN", {
                   maximumFractionDigits: 2,
-                  minimumFractionDigits: 2
+                  minimumFractionDigits: 2,
                 })}
               </div>
             </div>
